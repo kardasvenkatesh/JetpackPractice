60dfb4cca9c458042c6fb49c99cd6362fe1e6ab4
kardas_venkatesh <kardas.venkatesh@indianpac.com>
2023-08-16 16:55:00 +0530

    CASUAL COMMIT (16-08-2023,16:54)

--- app/src/main/java/com/kardas/jetpackprofilepractice/MainActivity.kt
+++ app/src/main/java/com/kardas/jetpackprofilepractice/MainActivity.kt
@@ -36,10 +36,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        AppCenter.start(
-            application, "25d803e0-0a60-4753-b71f-7356663d4f53",
-            Analytics::class.java, Crashes::class.java
-        )
+//        AppCenter.start(
+//            application, "25d803e0-0a60-4753-b71f-7356663d4f53",
+//            Analytics::class.java, Crashes::class.java
+//        )
 
         setContent {
             JetpackExampleTheme() {
